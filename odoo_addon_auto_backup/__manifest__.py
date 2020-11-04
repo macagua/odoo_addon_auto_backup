e96f0dba0201dcc5d22fa0ff5e29e506a30e71bf
Nghia Tran <tnghia99@gmail.com>
2020-11-04 14:27:05 +0700

    ODOO_RM_INTERN-026: description

--- odoo_addon_auto_backup/__manifest__.py
+++ odoo_addon_auto_backup/__manifest__.py
@@ -10,7 +10,7 @@ Odoo Addon Auto Backup
 Data is always an important aspect of any operational system. With
 this module, your Odoo database is safe. The module helps back up
 your database on a schedule that you set, so you always have
-a fallback should anything unexpected occur.
+a fallback plan should anything unexpected occur.
 
 
 Main Features
